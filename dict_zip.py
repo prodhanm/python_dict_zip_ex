482fe388071d4b05b3006bf414d2d81d521b4c45
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-03 14:23:26 -0400

    bound the values in report format.

--- dict_zip.py
+++ dict_zip.py
@@ -2,18 +2,11 @@ months = ["January", "February", "March", "April", "May", "June", \
           "July", "August", "Septmeber", "October", "Nomvember", \
             "December"]
 
-revenue = [32,000, 48,000, 72,000, 96,000, 120,000, 144,000, \
+revenues = [32,000, 48,000, 72,000, 96,000, 120,000, 144,000, \
            168,000, 192,000, 216,000, 240,000, 264,000, 2888,000]
 
-profit = [5,000, 10,000, 15,000, 20,000, 25,000, 30,000, \
+profits = [5,000, 10,000, 15,000, 20,000, 25,000, 30,000, \
            35,000, 40,000, 45,000, 50,000, 55,000, 60,000]
-
-def month_idx(months):
-    for idx, month in enumerate(months,1):
-        print(f"{idx}. {month}")
-    
-def main():
-    month_idx(months)
-
-if __name__ == "__main__":
-    main()
+print("Months|   Revenues  | Profits")
+for month, rev, prof in zip(months,revenues,profits):
+    print(f"{month}   {rev*1000}         {prof*1000}")
